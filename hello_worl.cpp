096070e602114441550dc622f10f667eaeaac849
orion <asiskrich@gmail.com>
2024-11-19 09:00:09 +0300

    preobrazovanie 0_0

--- hello_worl.cpp
+++ hello_worl.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-using namespace std;
 int main()
 { 
     //lublu jit
     string name;
+<<<<<<< HEAD
     cin>>name;
     cout << "Hello from "<<name;
+=======
+    std::cin>>name;
+    std::cout << "Hello from "<<name;
+>>>>>>> c44e40a (preobrazovanie 0_0)
 
     return 0;
 }
